fc47fd4faced57660e76eb0c0ad0810942008078
Thodoris Lazaris <117118038+Lazaris02@users.noreply.github.com>
2024-04-26 19:17:36 +0300

    added the code needed for preprocessing before the analyzing stage.

---
+++ txtparsing/DocumentData.java
@@ -0,0 +1,30 @@
+package txtparsing;
+
+public class DocumentData {
+    /*This class will store the data for each Document*/
+    private String id;
+    private String body;
+
+    public DocumentData(String id,String body){
+        this.id = id;
+        this.body = body;
+    }
+
+    /*Setters and Getters*/
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+}
---
+++ txtparsing/TxtParsing.java
@@ -0,0 +1,25 @@
+package  txtparsing;
+import utils.IO;
+
+import javax.print.Doc;
+import java.util.ArrayList;
+import java.util.List;
+
+public class TxtParsing{
+
+    /**
+     * reads the file + splits + puts it in an id-body format
+     * @param file The filename
+     * @return an ArrayList with the documents in the txt*/
+    public static List<DocumentData> parse(String file){
+        String txt_file = IO.txtToString(file); //entire file to string
+        String[] docs = txt_file.split("///"); // id and body combined
+
+        List<DocumentData> docData = new ArrayList<DocumentData>();
+        for(String doc : docs){
+            String[] split_docs = doc.split("\n",1);
+            docData.add(new DocumentData(split_docs[0],split_docs[1]));
+        }
+        return docData;
+    }
+}
